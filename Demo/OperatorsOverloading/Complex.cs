f419b935aeaa76dee1dc314f2107f4088572446b
Mohamed Elsayed <78699419+M-Elsayed9@users.noreply.github.com>
2024-08-19 23:53:00 +0300

    Operators Overloading - Binary operators

---
+++ Demo/OperatorsOverloading/Complex.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.OperatorsOverloading
+{
+    internal class Complex
+    {
+        // 10 + 6i
+        public int Real { get; set; }
+        public int Imag { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Real} + {Imag}i";
+        }
+
+        #region Operator Overloaidng - Binary Operators
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            return new Complex()
+            {
+                Real = (c1?.Real ?? 0) + (c2?.Real ?? 0),
+                Imag = (c1?.Imag ?? 0) + (c2?.Imag ?? 0)
+            };
+        }
+
+        public static Complex operator -(Complex c1, Complex c2)
+        {
+            return new Complex()
+            {
+                Real = (c1?.Real ?? 0) - (c2?.Real ?? 0),
+                Imag = (c1?.Imag ?? 0) - (c2?.Imag ?? 0)
+            };
+        } 
+        #endregion
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace Demo
+﻿
+using Demo.OperatorsOverloading;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Operator Overloading - Binary Operators
+            Complex c1 = new Complex() { Real = 10, Imag = 6 };
+            Console.WriteLine(c1);
+
+            Complex c2 = new Complex() { Real = 5, Imag = 3 };
+            Console.WriteLine(c2);
+
+            Complex c3 = default; // Null 
+
+            //c3 = c1 + c2; 
+
+            //Console.WriteLine(c3);
+
+            //c3 = c1 - c2;
+
+            c3 += c1;
+            c3 -= c2;
+
+            Console.WriteLine(c3);
+
+            #endregion
         }
     }
 }
