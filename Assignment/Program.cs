f5aedcfba6f747a88d1c3acba930b077d8998b54
Mohamed Elsayed <78699419+M-Elsayed9@users.noreply.github.com>
2024-08-26 03:34:17 +0300

    Project 01

---
+++ Assignment/Program.cs
@@ -0,0 +1,84 @@
+﻿using OOP05.Project1;
+
+
+namespace OOP05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            
+            //_3DPoint[] points =
+            //{
+            //    new _3DPoint(),
+            //    new _3DPoint()
+            //};
+
+            //for(int i = 0; i < 2; i++)
+            //{
+            //    Console.WriteLine($"Enter Cordinates of Point number {i+1}: ");
+            //    Console.Write("Enter X:");
+            //    points[i].X = int.Parse(Console.ReadLine());
+
+            //    Console.Write("Enter Y:");
+            //    points[i].Y = Convert.ToInt32(Console.ReadLine());
+
+            //    int z;
+
+            //    do
+            //    {
+            //        Console.Write("Enter Z:");
+            //    } while (!int.TryParse(Console.ReadLine(), out z));
+            //    points[i].Z = z;
+            //} 
+            
+
+
+            //_3DPoint P1 = new _3DPoint();
+            //_3DPoint P2 = new _3DPoint();
+
+            //P1.ReadPoint();
+
+            //P2.ReadPoint();
+
+            //if(P1 == P2)
+            //{
+            //    Console.WriteLine("EQUALS");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("!EQUALS");
+            //}
+
+            //_3DPoint[] points =
+            //{
+            //    new _3DPoint(),
+            //    new _3DPoint(),
+            //    new _3DPoint(),
+            //    new _3DPoint(),
+            //    new _3DPoint(),
+            //    new _3DPoint(),
+            //    new _3DPoint(),
+            //    new _3DPoint(),
+            //    new _3DPoint()
+            //};
+
+            //for(int i=0;i<points.Length;i++)
+            //{
+            //    points[i].ReadPoint();
+            //}
+
+            //Array.Sort(points);
+
+            //for (int i = 0; i < points.Length; i++)
+            //{
+            //    Console.WriteLine(points[i]);
+            //} 
+            #endregion
+
+            
+
+        }
+    }
+}
